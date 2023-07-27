cf1b51257c2ada42074bdf90096c8d9c850ff6d7
MostafaMohamedTaha <88216249+MostafaMohamedTaha@users.noreply.github.com>
2023-07-27 12:10:22 +0300

    third part input

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
 //((((((((((((((((html5))))))))))))))))
 //DOCTYPE  xhtml
+// html5 audio,video ,canvas,date,calender,
+//progress max="100" value="90"
+//meter min="18" max="55" value="39" high="45" low="28"
+//mark high light
+//form for in url name
+//input type="color"
+// type search ,number,reset,url,email,submit,required ,max, min,checkbox,radio
